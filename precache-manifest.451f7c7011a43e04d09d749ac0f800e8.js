55518f0950b38473ed7666231599b794902b526b
Kaiwen Luo <luokw8@gmail.com>
2019-11-20 01:01:03 +1100

    Updates

---
+++ precache-manifest.451f7c7011a43e04d09d749ac0f800e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bcbb45a12e9851309a7f801db62609",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc48996f9eeaf6a55211",
+    "url": "./static/css/main.b39bca4f.chunk.css"
+  },
+  {
+    "revision": "c8dcba0d746035bf2c5d",
+    "url": "./static/js/2.c556b40b.chunk.js"
+  },
+  {
+    "revision": "bc48996f9eeaf6a55211",
+    "url": "./static/js/main.185945bf.chunk.js"
+  },
+  {
+    "revision": "6b9e9cb664dfc043848d",
+    "url": "./static/js/runtime-main.e51d43ff.js"
+  },
+  {
+    "revision": "894d496e0dbbbee2689198f9d731d94f",
+    "url": "./static/media/bg.894d496e.png"
+  }
+]);
